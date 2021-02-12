50318216d69dd9a2a4b252c0aa4f329b6ab191b5
Dale Ellis <daleellis1983@gmail.com>
2021-02-12 17:08:52 +0000

    Added bootstrap

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,13 +1,21 @@
 import Vue from 'vue';
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
 import App from './App.vue';
 import router from './router';
 import {store} from './store';
 import VueSocketIO from 'vue-socket.io';
 
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import './main.scss';
+
 const API_SERVER_URL = process.env.VUE_APP_ROOT_API ? process.env.VUE_APP_ROOT_API : 'http://localhost:5000';
 
 Vue.config.productionTip = false;
 
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
 Vue.use(new VueSocketIO({
     connection: API_SERVER_URL,
     vuex: {
